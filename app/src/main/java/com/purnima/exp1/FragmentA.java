4a51b627ef28b6ae3736d9c2f3ff67a2af434add
PurnimaLimbu <pur.limbu@gmail.com>
2017-08-24 01:08:39 -0600

    fragmentA
    fragment A's xml done

---
+++ app/src/main/java/com/purnima/exp1/FragmentA.java
@@ -0,0 +1,25 @@
+package com.purnima.exp1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by purnimalimbu on 2017-08-24.
+ */
+
+public class FragmentA extends Fragment {
+    public FragmentA() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_a, container, false);
+    }
+}
