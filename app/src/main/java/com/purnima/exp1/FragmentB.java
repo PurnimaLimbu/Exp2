6bbbef062accae9b8630e2d03888fdf23d103bf4
PurnimaLimbu <pur.limbu@gmail.com>
2017-08-25 15:23:26 -0600

    fragments_layout
    fragments layout done

--- app/src/main/java/com/purnima/exp1/FragmentB.java
+++ app/src/main/java/com/purnima/exp1/FragmentB.java
@@ -2,7 +2,7 @@ package com.purnima.exp1;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentB extends Fragment {
-
 
     public FragmentB() {
         // Required empty public constructor
--- app/src/main/java/com/purnima/exp1/MainActivity.java
+++ app/src/main/java/com/purnima/exp1/MainActivity.java
@@ -1,13 +1,28 @@
 package com.purnima.exp1;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //LayoutFragments into containers
+        FragmentTransaction mFragmentTransaction = getFragmentManager().beginTransaction();
+        //FragA
+        FragmentA mFragmentA = new FragmentA();
+        mFragmentTransaction.replace(R.id.containerA,mFragmentA);
+
+        //FragB
+        FragmentB mFragmentB = new FragmentB();
+        mFragmentTransaction.replace(R.id.containerB,mFragmentB);
+
+        mFragmentTransaction.commit();
+
     }
 }
